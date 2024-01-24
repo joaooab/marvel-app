74b3d959389a9817f39ee7b9d85d2e563bde6cb4
joaofreitas <joaoovfreitas@gmail.com>
2024-01-24 00:19:58 -0300

    feature: add splash screen

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,7 +50,7 @@ dependencies {
 
     implementation("androidx.paging:paging-runtime-ktx:3.2.1")
     implementation("com.github.bumptech.glide:glide:4.12.0")
-
+    implementation("androidx.core:core-splashscreen:1.0.1")
     implementation("com.facebook.shimmer:shimmer:0.5.0")
 
     implementation("io.insert-koin:koin-android:3.5.3")
--- app/src/main/java/com/example/marvelapp/MainActivity.kt
+++ app/src/main/java/com/example/marvelapp/MainActivity.kt
@@ -2,10 +2,12 @@ package com.example.marvelapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        installSplashScreen()
     }
 }
