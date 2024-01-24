9857d459d712a0bb44b247f97afd0aaa0a94543f
joaofreitas <joaoovfreitas@gmail.com>
2024-01-24 01:23:36 -0300

    improvement: change colors and add toolbar

--- app/src/main/java/com/example/marvelapp/MainActivity.kt
+++ app/src/main/java/com/example/marvelapp/MainActivity.kt
@@ -1,13 +1,32 @@
 package com.example.marvelapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
+import com.example.marvelapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setupNavController(binding)
         installSplashScreen()
+    }
+
+    private fun setupNavController(binding: ActivityMainBinding) {
+        val navController = (supportFragmentManager.findFragmentById(R.id.navHostContainer) as NavHostFragment).navController
+        val appBarConfiguration = AppBarConfiguration(navController.graph)
+        binding.toolbarApp.setupWithNavController(navController, appBarConfiguration)
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            val isTopLevelDestinations = appBarConfiguration.topLevelDestinations.contains(destination.id)
+            if (!isTopLevelDestinations) {
+                binding.toolbarApp.setNavigationIcon(R.drawable.arrow_back)
+            }
+        }
     }
 }
